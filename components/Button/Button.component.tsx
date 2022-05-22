689fc6c0125eb5deaf7f27ac76e5646ec1caecf2
Paul Singh <im.makingstuffs@gmail.com>
2022-05-22 10:03:59 +0100

    Added a placeholder prop interface for the button component and spread in the props

--- components/Button/Button.component.tsx
+++ components/Button/Button.component.tsx
@@ -1,3 +1,7 @@
-const Button: React.FC = () => <button />;
+import { HTMLAttributes } from "react";
+
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {}
+
+const Button: React.FC<ButtonProps> = (props) => <button {...props} />;
 
 export { Button };
