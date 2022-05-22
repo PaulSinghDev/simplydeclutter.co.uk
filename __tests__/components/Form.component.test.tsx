b30e48fb6ef933f8d78ff3256079426a572fe370
Paul Singh <im.makingstuffs@gmail.com>
2022-05-22 10:28:18 +0100

    Add basic tests for the form component

--- __tests__/components/Form.component.test.tsx
+++ __tests__/components/Form.component.test.tsx
@@ -1,6 +1,30 @@
 import { Form } from "components/Form/Form.component";
 import { render, screen } from "utils";
 
+const CHILD_IDS = ["child1", "child2"];
+const TEST_ID = "Form";
+const FORM_COMPONENT = (
+  <Form data-testid={TEST_ID}>
+    <span data-testid={CHILD_IDS[0]}></span>
+    <span data-testid={CHILD_IDS[1]}></span>
+  </Form>
+);
+
 describe("<Form />", () => {
-  it("will render in the dom", () => {});
+  beforeEach(() => {
+    render(FORM_COMPONENT);
+  });
+
+  it("will render in the dom", () => {
+    expect(screen.getByTestId(TEST_ID)).toBeInTheDocument();
+  });
+
+  it("will render child components", () => {
+    const child1 = screen.getByTestId(CHILD_IDS[0]);
+    const child2 = screen.getByTestId(CHILD_IDS[1]);
+    expect(child1).toBeInTheDocument();
+    expect(child2).toBeInTheDocument();
+    expect(child1.closest(`[data-testid="${TEST_ID}"]`)).toBeDefined();
+    expect(child2.closest(`[data-testid="${TEST_ID}"]`)).toBeDefined();
+  });
 });
