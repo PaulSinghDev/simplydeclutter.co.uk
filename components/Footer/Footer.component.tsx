7687d9d510a4acfebaedfa6d98342237aa3ddb93
Paul Singh <im.makingstuffs@gmail.com>
2022-05-22 10:26:16 +0100

    Add styled components and typings

--- components/Footer/Footer.component.tsx
+++ components/Footer/Footer.component.tsx
@@ -1,3 +1,10 @@
-const Footer: React.FC = () => <footer />;
+import { HTMLAttributes } from "react";
+import styled from "styled-components";
+
+interface FooterProps extends HTMLAttributes<HTMLDivElement> {}
+
+const Footer: React.FC<FooterProps> = (props) => <StyledFooter {...props} />;
+
+const StyledFooter = styled.div``;
 
 export { Footer };
