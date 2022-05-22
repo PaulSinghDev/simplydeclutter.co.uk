3540848ab0dabf2f9d6af54e118e8713e3b8c44a
Paul Singh <im.makingstuffs@gmail.com>
2022-05-22 10:28:24 +0100

    Add styled components and typings

--- components/Form/Form.component.tsx
+++ components/Form/Form.component.tsx
@@ -1,3 +1,10 @@
-const Form: React.FC = () => <form />;
+import { HTMLAttributes } from "react";
+import styled from "styled-components";
+
+interface FormProps extends HTMLAttributes<HTMLFormElement> {}
+
+const Form: React.FC<FormProps> = (props) => <StyledForm {...props} />;
+
+const StyledForm = styled.form``;
 
 export { Form };
