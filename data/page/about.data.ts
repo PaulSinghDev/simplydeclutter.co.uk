1a15d7604d0a9c57ccec59fa5a7c0882626fb177
Paul Singh <im.makingstuffs@gmail.com>
2022-05-28 18:38:46 +0100

    Added some basic page data for SEO purposes

---
+++ data/page/about.data.ts
@@ -0,0 +1,8 @@
+import { PageData } from "types";
+
+const about: PageData = {
+  title: "This is my about",
+  description: "This is some information about my about page",
+};
+
+export default about;
---
+++ data/page/index.ts
@@ -0,0 +1,7 @@
+import prices from "./prices.data";
+import services from "./services.data";
+import index from "./index.data";
+import about from "./about.data";
+import contact from "./contact.data";
+
+export { prices, services, index, about, contact };
