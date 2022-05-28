94b8b3c5b29c90b1a18b65f9c42e78c2ea592ef5
Paul Singh <im.makingstuffs@gmail.com>
2022-05-28 18:42:05 +0100

    completed some basic implementation of a nav bar.
    The component takes an array of NavLink objects and iterates them to add in links. It still needs to add the logo etc but that is styling so after

--- components/Nav/Nav.component.tsx
+++ components/Nav/Nav.component.tsx
@@ -1,9 +1,51 @@
+import { Link } from "components";
 import { HTMLAttributes } from "react";
 import styled from "styled-components";
 
-interface NavProps extends HTMLAttributes<HTMLDivElement> {}
+export interface NavLinkInterface {
+  label: string;
+  title: string;
+  url: string;
+  subLinks?: NavLinkInterface[];
+  isExternal: boolean;
+}
 
-const Nav: React.FC<NavProps> = (props) => <StyledNav {...props} />;
+interface NavProps extends HTMLAttributes<HTMLDivElement> {
+  links: NavLinkInterface[];
+}
+
+const Nav: React.FC<NavProps> = ({ links, ...rest }) => {
+  return (
+    <StyledNav {...rest}>
+      {links.map((link: NavLinkInterface, i: number) => {
+        return (
+          <Link
+            key={`${Date.now()}-${i}-${link.label}`}
+            url={link.url}
+            title={link.title}
+          >
+            <>
+              <span>{link.label}</span>
+              {!!link.subLinks && link.subLinks.length > 0 ? (
+                <ul>
+                  {link.subLinks.map((subLink, i) => (
+                    <Link
+                      key={`${Date.now()}-${i}-${subLink.label}`}
+                      url={subLink.url}
+                      title={subLink.title}
+                    >
+                      {subLink.label}
+                    </Link>
+                  ))}
+                </ul>
+              ) : null}
+            </>
+          </Link>
+        );
+      })}
+    </StyledNav>
+  );
+};
 
 const StyledNav = styled.nav``;
 
