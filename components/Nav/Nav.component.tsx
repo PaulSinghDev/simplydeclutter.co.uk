870335cb2e5906d2353d6acc48d08da391d8c47f
Paul Singh <im.makingstuffs@gmail.com>
2022-05-28 22:33:41 +0100

    Finish an unstyled version of the nac [2 - 6] -- Import deps [21 - 34] -- Use hooks to allow us to set the nav's state in regard to whether or not the links should be displayed. On mobile we want a proper nav and on desktop we want a horizontal one
    [38 - 72] -- Add HTML structure for the nav
    [77 - 119] -- Add styling to handle the state of the links

--- components/Nav/Nav.component.tsx
+++ components/Nav/Nav.component.tsx
@@ -1,6 +1,9 @@
 import { Link } from "components";
-import { HTMLAttributes } from "react";
+import { Button } from "components/Button";
+import { HTMLAttributes, MouseEventHandler, useEffect, useState } from "react";
 import styled from "styled-components";
+import { useDeviceSize } from "hooks";
+import { IoMdMenu } from "react-icons/io";
 
 export interface NavLinkInterface {
   label: string;
@@ -15,38 +18,104 @@ interface NavProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 const Nav: React.FC<NavProps> = ({ links, ...rest }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { width } = useDeviceSize();
+
+  useEffect(() => {
+    if (window.innerWidth > 600) {
+      setIsOpen(true);
+    } else {
+      setIsOpen(false);
+    }
+  }, [width]);
+
+  const handleToggle: MouseEventHandler<HTMLButtonElement> = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <StyledNav {...rest}>
-      {links.map((link: NavLinkInterface, i: number) => {
-        return (
-          <Link
-            key={`${Date.now()}-${i}-${link.label}`}
-            url={link.url}
-            title={link.title}
-          >
-            <>
-              <span>{link.label}</span>
-              {!!link.subLinks && link.subLinks.length > 0 ? (
-                <ul>
-                  {link.subLinks.map((subLink, i) => (
-                    <Link
-                      key={`${Date.now()}-${i}-${subLink.label}`}
-                      url={subLink.url}
-                      title={subLink.title}
-                    >
-                      {subLink.label}
-                    </Link>
-                  ))}
-                </ul>
-              ) : null}
-            </>
-          </Link>
-        );
-      })}
+      <div className="nav-buttons">
+        <Button
+          className="burger-menu"
+          aria-label="Toggle the navigation menu"
+          onClick={handleToggle}
+        >
+          <IoMdMenu size={30} />
+        </Button>
+      </div>
+      <div className="nav-links" aria-expanded={isOpen}>
+        <ul>
+          {links.map((link: NavLinkInterface, i: number) => {
+            return (
+              <li key={`${Date.now()}-${i}-${link.label}`}>
+                <Link url={link.url} title={link.title}>
+                  <>
+                    <span>{link.label}</span>
+                    {!!link.subLinks && link.subLinks.length > 0 ? (
+                      <ul>
+                        {link.subLinks.map((subLink, i) => (
+                          <li key={`${Date.now()}-${i}-${subLink.label}`}>
+                            <Link url={subLink.url} title={subLink.title}>
+                              {subLink.label}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    ) : null}
+                  </>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </StyledNav>
   );
 };
 
-const StyledNav = styled.nav``;
+const StyledNav = styled.nav`
+  display: flex;
+  flex-wrap: wrap;
+
+  .nav-links {
+    width: 100%;
+
+    &[aria-expanded="false"] {
+      display: none;
+    }
+
+    ul {
+      margin: 0;
+      list-style: none;
+      padding: 0;
+      display: flex;
+      flex-direction: column;
+
+      li {
+        margin: 8px;
+        a {
+          padding: 12px 16px;
+        }
+      }
+    }
+  }
+
+  .nav-buttons {
+    margin-left: auto;
+  }
+
+  @media screen and (min-width: 600px) {
+    .nav-links {
+      width: auto;
+      ul {
+        flex-direction: row;
+      }
+    }
+    .nav-buttons {
+      display: none;
+    }
+  }
+`;
 
 export { Nav };
