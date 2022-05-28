2f4367d9edd22403c0362dbbf4b03fc3c90ff01e
Paul Singh <im.makingstuffs@gmail.com>
2022-05-28 13:22:55 +0100

    Add email and validation [6] -- Add 'email' as a type prop def [11] -- Get the isValid boolean from the hook [24] -- Implement the isValid boolean

--- components/Input/Input.component.tsx
+++ components/Input/Input.component.tsx
@@ -3,22 +3,25 @@ import React, { HTMLAttributes } from "react";
 import styled from "styled-components";
 
 interface InputProps extends HTMLAttributes<HTMLInputElement> {
-  type: "text" | "number";
+  type: "text" | "number" | "email";
   initialValue: string | number;
 }
 
 const Input: React.FC<InputProps> = ({ type, initialValue, ...rest }) => {
-  const { value, handleInput } = useInput(type, initialValue);
+  const { value, handleInput, isValid } = useInput(type, initialValue);
+
   const handleChangeEvent: React.ChangeEventHandler = (
     e: React.ChangeEvent<HTMLInputElement>
   ) => {
     handleInput(e.currentTarget.value);
   };
+
   return (
     <StyledInput
       type={type}
       value={value}
       onChange={handleChangeEvent}
+      data-valid={isValid}
       {...rest}
     />
   );
