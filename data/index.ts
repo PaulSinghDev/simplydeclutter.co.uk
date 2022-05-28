9cf07e10123f43a87d8b30d515fb5461bfcc1ebe
Paul Singh <im.makingstuffs@gmail.com>
2022-05-28 18:39:46 +0100

    Added data to the exporter

---
+++ data/index.ts
@@ -0,0 +1,6 @@
+import companyInfo from "./companyInfo.data";
+import siteInfo from "./siteInfo.data";
+import * as pages from "./page";
+import { navLinks } from "./nav";
+
+export { companyInfo, siteInfo, pages, navLinks };
