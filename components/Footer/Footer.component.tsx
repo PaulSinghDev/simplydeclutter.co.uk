aff60dc54ea96568c22b6cf63081f0f9cd388244
Paul Singh <im.makingstuffs@gmail.com>
2022-05-28 23:40:40 +0100

    Add some basic list element to the footer

--- components/Footer/Footer.component.tsx
+++ components/Footer/Footer.component.tsx
@@ -1,9 +1,67 @@
+import { Link } from "components";
 import { HTMLAttributes } from "react";
+
 import styled from "styled-components";
 
 interface FooterProps extends HTMLAttributes<HTMLDivElement> {}
 
-const Footer: React.FC<FooterProps> = (props) => <StyledFooter {...props} />;
+const Footer: React.FC<FooterProps> = (props) => {
+  return (
+    <StyledFooter {...props}>
+      <div className="footer-column">
+        <picture>
+          <img src="/assets/images/logos/full-logo-white.png" />
+        </picture>
+      </div>
+      <div className="footer-column">
+        <h4>Useful Links</h4>
+        <ul>
+          <li>
+            <Link url="/about" title="Go to about page">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link url="/contact" title="Go to contact page">
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link url="/services" title="Go to services page">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link url="/prices" title="Go to prices page">
+              Prices
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-column">
+        <h4>Legal Information</h4>
+        <ul>
+          <li>
+            <Link url="/privacy-policy" title="Go to privacy policy">
+              Privacy Policy
+            </Link>
+          </li>
+          <li>
+            <Link url="//terms-of-use" title="Go to terms of use">
+              Terms of Use
+            </Link>
+          </li>
+          <li>
+            <Link url="/terms-of-service" title="Go to terms of service">
+              Terms of Service
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-column"></div>
+    </StyledFooter>
+  );
+};
 
 const StyledFooter = styled.div``;
 
