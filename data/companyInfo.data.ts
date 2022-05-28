5f1094995b767e6d39bfc95861326342476e47e7
Paul Singh <im.makingstuffs@gmail.com>
2022-05-28 18:39:16 +0100

    Added some dummy company data

---
+++ data/companyInfo.data.ts
@@ -0,0 +1,12 @@
+import { CompanyInfo } from "types";
+
+const companyInfo: CompanyInfo = {
+  businessName: "Example Business",
+  tagLine: "We are a business and we do things well",
+  phone: "123123123",
+  address: ["123 Fake street", "Fake Town", "Fake city"],
+  logoPath: "/assets/images/logo.png",
+  owner: "The Boss",
+};
+
+export default companyInfo;
