4e53903fc048eb254b704c714639416edd62fb9e
Paul Singh <im.makingstuffs@gmail.com>
2022-05-28 18:43:03 +0100

    Updated the nav tests so that is now checks sublinks and links

--- __tests__/components/Nav.component.test.tsx
+++ __tests__/components/Nav.component.test.tsx
@@ -1,30 +1,76 @@
-import { Nav } from "components/Nav/Nav.component";
+import { Nav, NavLinkInterface } from "components/Nav/Nav.component";
 import { render, screen } from "utils";
 
 const CHILD_IDS = ["child1", "child2"];
 const TEST_ID = "Nav";
-const NAV_COMPONENT = (
-  <Nav data-testid={TEST_ID}>
-    <button data-testid={CHILD_IDS[0]}></button>
-    <button data-testid={CHILD_IDS[1]}></button>
-  </Nav>
+const SINGLE_TIER_NAV_LINKS: NavLinkInterface[] = [
+  {
+    label: "link 1",
+    url: "/about",
+    title: "this is a link",
+    isExternal: false,
+  },
+  {
+    label: "link 2",
+    url: "/contact",
+    title: "this is a link",
+    isExternal: false,
+  },
+];
+const TWO_TIER_NAV_LINKS: NavLinkInterface[] = [
+  {
+    label: "link 1",
+    url: "/about",
+    title: "this is a link",
+    isExternal: false,
+    subLinks: [
+      {
+        label: "sub link 1",
+        url: "/about",
+        title: "this is a link",
+        isExternal: false,
+      },
+      {
+        label: "sub link 2",
+        url: "/about",
+        title: "this is a link",
+        isExternal: false,
+      },
+    ],
+  },
+];
+const NAV_COMPONENT_NO_SUB_LINKS = (
+  <Nav data-testid={TEST_ID} links={SINGLE_TIER_NAV_LINKS}></Nav>
+);
+const NAV_COMPONENT_SUB_LINKS = (
+  <Nav data-testid={TEST_ID} links={TWO_TIER_NAV_LINKS}></Nav>
 );
 
+const setupTest = (isTwoTier: boolean = false) => {
+  const utils = render(
+    isTwoTier ? NAV_COMPONENT_SUB_LINKS : NAV_COMPONENT_NO_SUB_LINKS
+  );
+  const element = utils.getByTestId(TEST_ID);
+  return { element, utils };
+};
+
 describe("<Nav />", () => {
-  beforeEach(() => {
-    render(NAV_COMPONENT);
-  });
-
   it("will render in the dom", () => {
-    expect(screen.getByTestId(TEST_ID)).toBeInTheDocument();
+    const { element } = setupTest();
+    expect(element).toBeInTheDocument();
   });
 
-  it("will render child components", () => {
-    const child1 = screen.getByTestId(CHILD_IDS[0]);
-    const child2 = screen.getByTestId(CHILD_IDS[1]);
-    expect(child1).toBeInTheDocument();
-    expect(child2).toBeInTheDocument();
-    expect(child1.closest(`[data-testid="${TEST_ID}"]`)).toBeDefined();
-    expect(child2.closest(`[data-testid="${TEST_ID}"]`)).toBeDefined();
+  it("will render tier 1 links", () => {
+    const { utils } = setupTest();
+    SINGLE_TIER_NAV_LINKS.forEach((link) => {
+      expect(utils.getByText(link.label)).toBeInTheDocument();
+    });
+  });
+
+  it("will render tier two links", () => {
+    const { utils } = setupTest(true);
+    TWO_TIER_NAV_LINKS[0].subLinks?.forEach((link) => {
+      expect(utils.getByText(link.label)).toBeInTheDocument();
+    });
   });
 });
