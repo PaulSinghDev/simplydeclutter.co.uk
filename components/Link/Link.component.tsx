1ce588140f1c079241ef98b2f70c1a0b8f57142a
Paul Singh <im.makingstuffs@gmail.com>
2022-06-06 19:41:14 +0100

    Add the ability to pass a prop in order to use the styling of a button
    [7] -- Define props
    [11] -- Add prop with default of false
    [17] -- implement the prop
    [23 - 40] -- Add some CSS to style the link depending on the asButton prop

--- components/Link/Link.component.tsx
+++ components/Link/Link.component.tsx
@@ -4,14 +4,40 @@ import { HTMLAttributes } from "react";
 
 interface LinkProps extends HTMLAttributes<HTMLAnchorElement> {
   url: string;
+  asButton?: boolean;
 }
 
-const Link: React.FC<LinkProps> = ({ url, children, ...rest }) => (
+const Link: React.FC<LinkProps> = ({
+  asButton = false,
+  url,
+  children,
+  ...rest
+}) => (
   <NextLink href={url} passHref>
-    <StyledLink {...rest}>{children}</StyledLink>
+    <StyledLink asButton={asButton} {...rest}>
+      {children}
+    </StyledLink>
   </NextLink>
 );
 
-const StyledLink = styled.a``;
+const StyledLink = styled.a<{ asButton: boolean }>`
+  ${({ asButton }) => {
+    const buttonStyle = `
+      background-color: var(--blue);
+      color: #fff;
+      font-weight: bold;
+      padding: 12px 24px;
+      border-radius: 24px;
+      margin: 8px;
+      transition: 0.3s ease;
+
+      &:hover {
+        background-color: var(--off-black);
+      }
+    `;
+    const linkStyle = ``;
+    return asButton ? buttonStyle : linkStyle;
+  }}
+`;
 
 export { Link };
