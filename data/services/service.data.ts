846a6207881d6d8543b59cec3e4c79a09c2ed400
Paul Singh <im.makingstuffs@gmail.com>
2022-06-06 19:42:59 +0100

    Add some data for the services component

---
+++ data/services/service.data.ts
@@ -0,0 +1,16 @@
+import { Service } from "types";
+
+const services: Service[] = [
+  {
+    title: "Service 1",
+    description: "This is the service which we are offering here",
+    icon: "shirt",
+  },
+  {
+    title: "Service 2",
+    description: "This is another service which we are offering here",
+    icon: "iron",
+  },
+];
+
+export { services };
