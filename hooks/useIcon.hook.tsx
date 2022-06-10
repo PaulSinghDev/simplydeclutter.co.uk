af50bf694656e6bd652e44650c057a0d6c4ea045
Paul Singh <im.makingstuffs@gmail.com>
2022-06-10 06:54:49 +0100

    Add multiple new icons to the useIcon hook

--- hooks/useIcon.hook.tsx
+++ hooks/useIcon.hook.tsx
@@ -1,12 +1,28 @@
 import { MdIron } from "react-icons/md";
-import { GiShirt } from "react-icons/gi";
+import { GiShirt, GiPapers, GiHastyGrave } from "react-icons/gi";
+import { RiArchiveDrawerLine } from "react-icons/ri";
+import { ImShrink2 } from "react-icons/im";
+import { FaBaby, FaBoxOpen } from "react-icons/fa";
+import { Icons } from "types";
 
-const useIcon = (icon: "shirt" | "iron") => {
+const useIcon = (icon: Icons) => {
   switch (icon) {
     case "shirt":
       return <GiShirt />;
     case "iron":
       return <MdIron />;
+    case "drawer":
+      return <RiArchiveDrawerLine />;
+    case "shrink":
+      return <ImShrink2 />;
+    case "baby":
+      return <FaBaby />;
+    case "grave":
+      return <GiHastyGrave />;
+    case "docs":
+      return <GiPapers />;
+    case "box":
+      return <FaBoxOpen />;
     default:
       return <MdIron />;
   }
