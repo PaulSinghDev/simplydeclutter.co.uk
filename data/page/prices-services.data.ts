f4078be2590e4a4a3d65604d269d8c6b060703b8
Paul Singh <im.makingstuffs@gmail.com>
2022-06-30 20:38:01 +0100

    Add new images

---
+++ data/page/prices-services.data.ts
@@ -0,0 +1,10 @@
+import { PageData } from "types";
+
+const services: PageData = {
+  title:
+    "Prices & Services | Simply Declutter | Professionally Declutter and Organise Your Home",
+  description:
+    "If you are stressed out by your disorganised home or are struggling to let go of things you no longer need I can help you to regain control and create a calm, more organised home.",
+};
+
+export default services;
